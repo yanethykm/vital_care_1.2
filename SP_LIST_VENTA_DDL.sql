3e2a47c52d274131ece33de71e3686b1561b4e42
unknown <yanethykm@gmail.com>
2024-01-09 21:55:09 -0500

    Realizando modificación en procedimiento almacenado List de la tabla Venta debido a un error en la consulta

--- SP_LIST_VENTA_DDL.sql
+++ SP_LIST_VENTA_DDL.sql
@@ -3,19 +3,25 @@ IS
 BEGIN
     FOR rec IN (
         SELECT 
-            V.Fecha,
+            V.Fecha AS "Fecha",
             V.MontoTotal AS "Monto Total",
+            DV.CantidadVendida AS "Cantidad Vendida",
+            Pr.Producto AS "Producto",
+            Pr.PrecioVenta AS "Precio Venta",
             PCliente.nombre_razonsocial AS "Nombre Cliente",
             PCliente.apellido AS "Apellido Cliente",
             PCliente.telefono AS "Telefono Cliente",
             PCliente.direccion AS "Direccion Cliente",
             PCliente.email AS "Correo Cliente",
             PVendedor.nombre_razonsocial AS "Nombre Vendedor"
-        FROM Venta V
-        INNER JOIN Cliente C ON V.IdCliente = C.Id
-        INNER JOIN Vendedor Vd ON V.IdVendedor = Vd.Id 
-        INNER JOIN Persona PCliente ON C.IdPersona = PCliente.Id
-        INNER JOIN Persona PVendedor ON Vd.IdPersona = PVendedor.Id
+    FROM Venta V
+        INNER JOIN DetalleVenta DV ON V.id = DV.idventa  -- Corrección en el alias DVenta
+        INNER JOIN Producto Pr ON DV.idproducto = Pr.id   -- Corrección en el alias DVenta
+        INNER JOIN Cliente C ON V.idcliente = C.id
+        INNER JOIN Vendedor Ven ON V.idvendedor = Ven.id
+        INNER JOIN Persona PCliente ON C.idpersona = PCliente.id
+        INNER JOIN Persona PVendedor ON Ven.idpersona = PVendedor.id
+
     )
     LOOP
         DBMS_OUTPUT.PUT_LINE('Fecha: ' || TO_CHAR(rec.Fecha, 'DD-MON-YYYY HH24:MI:SS'));
