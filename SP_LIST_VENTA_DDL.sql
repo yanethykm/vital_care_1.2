d055ec069acaa54414b6f2fe17452362276e2fc3
unknown <yanethykm@gmail.com>
2024-01-11 12:19:56 -0500

    Modificando el procedimiento almacenado LIST de la tabla venta para que me muestre los datos del cliente, vendedor, el producto, la cantidad vendida y el total de la venta.

--- SP_LIST_VENTA_DDL.sql
+++ SP_LIST_VENTA_DDL.sql
@@ -1,9 +1,9 @@
 CREATE OR REPLACE PROCEDURE sp_List_Venta_DDL
 IS
 BEGIN
-    FOR rec IN (
+    FOR ventas IN (
         SELECT 
-            V.Fecha AS "Fecha",
+            V.fecha AS "Fecha",
             V.MontoTotal AS "Monto Total",
             DV.CantidadVendida AS "Cantidad Vendida",
             Pr.Producto AS "Producto",
@@ -14,24 +14,26 @@ BEGIN
             PCliente.direccion AS "Direccion Cliente",
             PCliente.email AS "Correo Cliente",
             PVendedor.nombre_razonsocial AS "Nombre Vendedor"
-    FROM Venta V
-        INNER JOIN DetalleVenta DV ON V.id = DV.idventa  -- Corrección en el alias DVenta
-        INNER JOIN Producto Pr ON DV.idproducto = Pr.id   -- Corrección en el alias DVenta
-        INNER JOIN Cliente C ON V.idcliente = C.id
-        INNER JOIN Vendedor Ven ON V.idvendedor = Ven.id
-        INNER JOIN Persona PCliente ON C.idpersona = PCliente.id
-        INNER JOIN Persona PVendedor ON Ven.idpersona = PVendedor.id
-
+        FROM Venta V
+            INNER JOIN DetalleVenta DV ON V.id = DV.idventa
+            INNER JOIN Producto Pr ON DV.idproducto = Pr.id
+            INNER JOIN Cliente C ON V.idcliente = C.id
+            INNER JOIN Vendedor Ven ON V.idvendedor = Ven.id
+            INNER JOIN Persona PCliente ON C.idpersona = PCliente.id
+            INNER JOIN Persona PVendedor ON Ven.idpersona = PVendedor.id
     )
     LOOP
-        DBMS_OUTPUT.PUT_LINE('Fecha: ' || TO_CHAR(rec.Fecha, 'DD-MON-YYYY HH24:MI:SS'));
-        DBMS_OUTPUT.PUT_LINE('Monto Total: ' || rec."Monto Total");
-        DBMS_OUTPUT.PUT_LINE('Nombre Cliente: ' || rec."Nombre Cliente");
-        DBMS_OUTPUT.PUT_LINE('Apellido Cliente: ' || rec."Apellido Cliente");
-        DBMS_OUTPUT.PUT_LINE('Telefono Cliente: ' || rec."Telefono Cliente");
-        DBMS_OUTPUT.PUT_LINE('Direccion Cliente: ' || rec."Direccion Cliente");
-        DBMS_OUTPUT.PUT_LINE('Correo Cliente: ' || rec."Correo Cliente");
-        DBMS_OUTPUT.PUT_LINE('Nombre Vendedor: ' || rec."Nombre Vendedor");
+        DBMS_OUTPUT.PUT_LINE('Fecha: ' || TO_CHAR(ventas."Fecha", 'DD-MON-YYYY HH24:MI:SS'));
+        DBMS_OUTPUT.PUT_LINE('Nombre Cliente: ' || ventas."Nombre Cliente");
+        DBMS_OUTPUT.PUT_LINE('Apellido Cliente: ' || ventas."Apellido Cliente");
+        DBMS_OUTPUT.PUT_LINE('Telefono Cliente: ' || ventas."Telefono Cliente");
+        DBMS_OUTPUT.PUT_LINE('Direccion Cliente: ' || ventas."Direccion Cliente");
+        DBMS_OUTPUT.PUT_LINE('Correo Cliente: ' || ventas."Correo Cliente");
+        DBMS_OUTPUT.PUT_LINE('Nombre Vendedor: ' || ventas."Nombre Vendedor");
+        DBMS_OUTPUT.PUT_LINE('Producto: ' || ventas."Producto");
+        DBMS_OUTPUT.PUT_LINE('Precio Venta: ' || ventas."Precio Venta");
+        DBMS_OUTPUT.PUT_LINE('Cantidad Vendida: ' || ventas."Cantidad Vendida");
+        DBMS_OUTPUT.PUT_LINE('Monto Total: ' || ventas."Monto Total");
         DBMS_OUTPUT.PUT_LINE('----------------------------------------------------------');
     END LOOP;
 END sp_List_Venta_DDL;
