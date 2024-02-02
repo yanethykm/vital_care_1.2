77059c844287ab150fc0f4112e9e47b59f1872a6
unknown <yanethykm@gmail.com>
2024-02-01 23:25:06 -0500

    Modificacion realizada a los procedimientos almacenados de la tabla persona para la visualización de los datos en la Presentación de Visual

--- PQ_PERSONA_PACKAGE.sql
+++ PQ_PERSONA_PACKAGE.sql
@@ -1,28 +1,33 @@
 CREATE OR REPLACE PACKAGE pq_Persona_Package AS
   -- Procedimiento para insertar una nueva persona
   PROCEDURE sp_Insert_Persona (
+        p_Cursor OUT SYS_REFCURSOR,
+        p_NitCedula IN INT,
+        p_NombreRazonSocial IN VARCHAR,
+        p_Apellido IN VARCHAR,
+        p_Direccion IN VARCHAR,
+        p_Telefono IN VARCHAR,
+        p_Email IN VARCHAR,
+        p_TipoDocumento IN VARCHAR
+        
+    );
+
+
+  -- Procedimiento para listar todas las persona
+  PROCEDURE sp_List_Persona(p_Cursor OUT SYS_REFCURSOR);
+
+  -- Procedimiento para actualizar una persona
+  PROCEDURE sp_Update_Persona (
+    p_Cursor OUT SYS_REFCURSOR,
+    p_ID IN INT,
     p_NitCedula IN INT,
     p_NombreRazonSocial IN VARCHAR,
     p_Apellido IN VARCHAR,
     p_Direccion IN VARCHAR,
     p_Telefono IN VARCHAR,
     p_Email IN VARCHAR,
-    p_TipoDocumento IN VARCHAR);
-
-  -- Procedimiento para listar todas las persona
-  PROCEDURE sp_List_Persona;
-
-  -- Procedimiento para actualizar una persona
-  PROCEDURE sp_Update_Persona (
-    p_ID IN INT,
-    p_NitCedula IN VARCHAR,
-    p_NombreRazonSocial IN VARCHAR,
-    p_Apellido IN VARCHAR,
-    p_Direccion IN VARCHAR,
-    p_Telefono IN VARCHAR,
-    p_Email IN VARCHAR,
-    p_TipoDocumento IN VARCHAR);
-
+    p_TipoDocumento IN VARCHAR
+);
   -- Procedimiento para eliminar una categoría por ID
   PROCEDURE sp_Delete_Persona (p_ID IN INT);
 END pq_Persona_Package;
@@ -33,6 +38,7 @@ END pq_Persona_Package;
 CREATE OR REPLACE PACKAGE BODY pq_Persona_Package AS
   -- Implementación del procedimiento para insertar una nueva persona
     PROCEDURE sp_Insert_Persona (
+        p_Cursor OUT SYS_REFCURSOR,
         p_NitCedula IN INT,
         p_NombreRazonSocial IN VARCHAR,
         p_Apellido IN VARCHAR,
@@ -44,32 +50,38 @@ CREATE OR REPLACE PACKAGE BODY pq_Persona_Package AS
     BEGIN
         INSERT INTO Persona (Nit_Cedula, Nombre_RazonSocial, Apellido, Direccion, Telefono, Email, TipoDocumento)
         VALUES (p_NitCedula, p_NombreRazonSocial, p_Apellido, p_Direccion, p_Telefono, p_Email, p_TipoDocumento);
+        
+        -- Abrir el cursor con los datos insertados
+        OPEN p_Cursor FOR
+            SELECT Nit_Cedula, Nombre_RazonSocial, Apellido, Direccion, Telefono, Email, TipoDocumento
+            FROM Persona
+            WHERE Nit_Cedula = p_NitCedula;
+    
         COMMIT;
     EXCEPTION
         WHEN OTHERS THEN
             ROLLBACK;
             RAISE;
+            
     END sp_Insert_Persona;
 
   -- Implementación del procedimiento para listar todas las personas
-    PROCEDURE sp_List_Persona AS
+    PROCEDURE sp_List_Persona(p_Cursor OUT SYS_REFCURSOR)AS
     BEGIN
-        FOR persona IN (SELECT Nit_Cedula, Nombre_RazonSocial, Apellido, Direccion, Telefono, Email, TipoDocumento FROM Persona) LOOP
-            DBMS_OUTPUT.PUT_LINE('Nit_Cedula: ' || persona.Nit_Cedula);
-            DBMS_OUTPUT.PUT_LINE('Nombre_RazonSocial: ' || persona.Nombre_RazonSocial);
-            DBMS_OUTPUT.PUT_LINE('Apellido: ' || persona.Apellido);
-            DBMS_OUTPUT.PUT_LINE('Direccion: ' || persona.Direccion);
-            DBMS_OUTPUT.PUT_LINE('Telefono: ' || persona.Telefono);
-            DBMS_OUTPUT.PUT_LINE('Email: ' || persona.Email);
-            DBMS_OUTPUT.PUT_LINE('TipoDocumento: ' || persona.TipoDocumento);
-            DBMS_OUTPUT.NEW_LINE;
-        END LOOP;
+        OPEN p_Cursor FOR    
+          SELECT Nit_Cedula as "Identificación", 
+          Nombre_RazonSocial as "Nombre / Razón Social", 
+          Apellido as "Apellido", 
+          Direccion as "Dirección", 
+          Telefono as "Telefono", 
+          Email as "Email", TipoDocumento as "Tipo de Documento" FROM Persona;
     END sp_List_Persona;
 
   -- Implementación del procedimiento para actualizar una persona
     PROCEDURE sp_Update_Persona (
+        p_Cursor OUT SYS_REFCURSOR,
         p_ID IN INT,
-        p_NitCedula IN VARCHAR,
+        p_NitCedula IN INT,
         p_NombreRazonSocial IN VARCHAR,
         p_Apellido IN VARCHAR,
         p_Direccion IN VARCHAR,
@@ -88,7 +100,10 @@ CREATE OR REPLACE PACKAGE BODY pq_Persona_Package AS
             Email = p_Email,
             TipoDocumento = p_TipoDocumento
         WHERE ID = p_ID;
-    
+        
+        OPEN p_Cursor FOR
+        SELECT ID, Nit_Cedula, Nombre_RazonSocial, Apellido, Direccion, Telefono, Email, TipoDocumento FROM Persona WHERE ID = p_ID;
+
         COMMIT;
     EXCEPTION
         WHEN OTHERS THEN
@@ -98,11 +113,16 @@ CREATE OR REPLACE PACKAGE BODY pq_Persona_Package AS
 
   -- Implementación del procedimiento para eliminar una persona
     PROCEDURE sp_Delete_Persona (
+        p_Cursor OUT SYS_REFCURSOR,
         p_ID IN INT
     ) AS
     BEGIN
         DELETE FROM Persona
         WHERE ID = p_ID;
+        
+        OPEN p_Cursor FOR
+        SELECT ID, Nit_Cedula, Nombre_RazonSocial, Apellido, Direccion, Telefono, Email, TipoDocumento FROM Persona WHERE ID = p_ID;
+        
         COMMIT;
     EXCEPTION
         WHEN OTHERS THEN
