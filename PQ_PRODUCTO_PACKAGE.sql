5816e7e5aed226ed2440881a2c38b451206cbf31
unknown <yanethykm@gmail.com>
2024-02-06 17:31:32 -0500

    Realizando modificaciones en los scripts de paquetes y procedimientos almancenados realizando una agregación de cursores para la muestra de datos en las capas de la tabla Producto

--- PQ_PRODUCTO_PACKAGE.sql
+++ PQ_PRODUCTO_PACKAGE.sql
@@ -1,32 +1,35 @@
 CREATE OR REPLACE PACKAGE pq_Producto_Package AS
   -- Procedimiento para insertar una nueva producto
   PROCEDURE sp_Insert_Producto (
-    p_IdProducto IN INT,
-    p_Producto IN VARCHAR,
-    p_Descripcion IN VARCHAR,
-    p_PrecioVenta IN INT,
-    p_CantidadMinima IN INT,
-    p_Cantidad IN INT,
-    p_IdCategoria IN INT,
-    p_IdProveedor IN INT);
+        p_Cursor OUT SYS_REFCURSOR,
+        p_IdProducto IN INT,
+        p_Producto IN VARCHAR,
+        p_Descripcion IN VARCHAR,
+        p_PrecioVenta IN INT,
+        p_CantidadMinima IN INT,
+        p_Cantidad IN INT,
+        p_IdCategoria IN INT,
+        p_IdProveedor IN INT
+    );
 
   -- Procedimiento para listar todos los productos
-  PROCEDURE sp_List_Producto_DDL;
+  PROCEDURE sp_List_Producto_DDL (p_Cursor OUT SYS_REFCURSOR);
 
   -- Procedimiento para actualizar un producto
   PROCEDURE sp_Update_Producto (
-    p_OldIdProducto IN INT,
-    p_NewIdProducto IN INT,
-    p_Producto IN VARCHAR,
-    p_Descripcion IN VARCHAR,
-    p_PrecioVenta IN INT,
-    p_CantidadMinima IN INT,
-    p_Cantidad IN INT,
-    p_IdCategoria IN INT,
-    p_IdProveedor IN INT);
+        p_Cursor OUT SYS_REFCURSOR,
+        p_OldIdProducto IN INT,
+        p_NewIdProducto IN INT,
+        p_Producto IN VARCHAR,
+        p_Descripcion IN VARCHAR,
+        p_PrecioVenta IN INT,
+        p_CantidadMinima IN INT,
+        p_Cantidad IN INT,
+        p_IdCategoria IN INT,
+        p_IdProveedor IN INT);
 
   -- Procedimiento para eliminar un producto
-  PROCEDURE sp_Delete_Producto (p_IdProducto IN INT);
+  PROCEDURE sp_Delete_Producto (p_Cursor OUT SYS_REFCURSOR, p_IdProducto IN INT);
 END pq_Producto_Package;
 -------------------------------------------------------------------------------
 -------------------------------------------------------------------------------
@@ -35,6 +38,7 @@ END pq_Producto_Package;
 CREATE OR REPLACE PACKAGE BODY pq_Producto_Package AS
   -- Implementación del procedimiento para insertar un producto
     PROCEDURE sp_Insert_Producto (
+        p_Cursor OUT SYS_REFCURSOR,
         p_IdProducto IN INT,
         p_Producto IN VARCHAR,
         p_Descripcion IN VARCHAR,
@@ -47,6 +51,10 @@ CREATE OR REPLACE PACKAGE BODY pq_Producto_Package AS
     BEGIN
         INSERT INTO Producto (ID, Producto, Descripcion, PrecioVenta, CantidadMinima, Cantidad, IdCategoria, IdProveedor)
         VALUES (p_IdProducto, p_Producto, p_Descripcion, p_PrecioVenta, p_CantidadMinima, p_Cantidad, p_IdCategoria, p_IdProveedor);
+        
+        -- Asignar el cursor de salida
+        OPEN p_Cursor FOR
+            SELECT * FROM Producto WHERE ID = p_IdProducto;
         COMMIT;
     EXCEPTION
         WHEN OTHERS THEN
@@ -55,10 +63,10 @@ CREATE OR REPLACE PACKAGE BODY pq_Producto_Package AS
     END sp_Insert_Producto;
 
   -- Implementación del procedimiento para listar todos los productos
-    PROCEDURE sp_List_Producto_DDL
+    PROCEDURE sp_List_Producto_DDL (p_Cursor OUT SYS_REFCURSOR)
     AS
     BEGIN
-        FOR producto IN (
+        OPEN p_Cursor FOR
             SELECT
                 p.id AS "Codigo Producto",
                 p.producto AS "Producto",
@@ -72,25 +80,12 @@ CREATE OR REPLACE PACKAGE BODY pq_Producto_Package AS
             FROM Producto p
             INNER JOIN Categoria c ON p.idcategoria = c.id
             INNER JOIN Proveedor pr ON p.idproveedor = pr.id
-            INNER JOIN Persona pv ON pr.idpersona = pv.id
-        ) LOOP
-            -- Imprimir o procesar cada fila según sea necesario
-            DBMS_OUTPUT.PUT_LINE(
-                'Codigo Producto: ' || producto."Codigo Producto" ||
-                ', Producto: ' || producto."Producto" ||
-                ', Descripcion: ' || producto."Descripcion" ||
-                ', Precio de Venta: ' || producto."Precio de Venta" ||
-                ', Cantidad Minima: ' || producto."Cantidad Minima" ||
-                ', Cantidad Existente: ' || producto."Cantidad Existente" ||
-                ', Categoria: ' || producto."Categoria" ||
-                ', Nombre del Proveedor: ' || producto."Nombre del Proveedor" ||
-                ', Apellido del Proveedor: ' || producto."Apellido del Proveedor"
-            );
-        END LOOP;
+            INNER JOIN Persona pv ON pr.idpersona = pv.id;
     END sp_List_Producto_DDL;
 
   -- Implementación del procedimiento para actualizar un producto
     PROCEDURE sp_Update_Producto (
+        p_Cursor OUT SYS_REFCURSOR,
         p_OldIdProducto IN INT,
         p_NewIdProducto IN INT,
         p_Producto IN VARCHAR,
@@ -114,7 +109,11 @@ CREATE OR REPLACE PACKAGE BODY pq_Producto_Package AS
             IdProveedor = p_IdProveedor
         WHERE
             ID = p_OldIdProducto;
-    
+        
+        -- Asignar el cursor de salida
+        OPEN p_Cursor FOR
+            SELECT * FROM Producto WHERE ID = p_NewIdProducto;
+            
         COMMIT;
     EXCEPTION
         WHEN OTHERS THEN
@@ -123,15 +122,17 @@ CREATE OR REPLACE PACKAGE BODY pq_Producto_Package AS
     END sp_Update_Producto;
 
   -- Implementación del procedimiento para eliminar un producto
-    PROCEDURE sp_Delete_Producto (p_IdProducto IN INT) AS
+    PROCEDURE sp_Delete_Producto (p_Cursor OUT SYS_REFCURSOR, p_IdProducto IN INT) AS
     BEGIN
+        -- Guardar datos del producto antes de eliminarlo
+        OPEN p_Cursor FOR
+            SELECT * FROM Producto WHERE ID = p_IdProducto;
+            
         DELETE FROM Producto
         WHERE ID = p_IdProducto;
-    
+
         COMMIT;
     EXCEPTION
-        WHEN NO_DATA_FOUND THEN
-            DBMS_OUTPUT.PUT_LINE('No se encontró un producto con el ID ' || p_IdProducto);
         WHEN OTHERS THEN
             ROLLBACK;
             RAISE;
--- SP_CRUD_TBL_PRODUCTO.sql
+++ SP_CRUD_TBL_PRODUCTO.sql
@@ -2,76 +2,85 @@
 ------------------- PROCEDIMIENTO ALMACENADO INSERT ---------------------------
 -------------------------------------------------------------------------------
 CREATE OR REPLACE PROCEDURE sp_Insert_Producto (
-    p_IdProducto IN INT,
-    p_Producto IN VARCHAR,
-    p_Descripcion IN VARCHAR,
-    p_PrecioVenta IN INT,
-    p_CantidadMinima IN INT,
-    p_Cantidad IN INT,
-    p_IdCategoria IN INT,
-    p_IdProveedor IN INT
-) AS
-BEGIN
-    INSERT INTO Producto (ID, Producto, Descripcion, PrecioVenta, CantidadMinima, Cantidad, IdCategoria, IdProveedor)
-    VALUES (p_IdProducto, p_Producto, p_Descripcion, p_PrecioVenta, p_CantidadMinima, p_Cantidad, p_IdCategoria, p_IdProveedor);
-    
-    COMMIT;
-EXCEPTION
-    WHEN OTHERS THEN
-        ROLLBACK;
-        RAISE;
-END sp_Insert_Producto;
+        p_Cursor OUT SYS_REFCURSOR,
+        p_IdProducto IN INT,
+        p_Producto IN VARCHAR,
+        p_Descripcion IN VARCHAR,
+        p_PrecioVenta IN INT,
+        p_CantidadMinima IN INT,
+        p_Cantidad IN INT,
+        p_IdCategoria IN INT,
+        p_IdProveedor IN INT
+    ) AS
+    BEGIN
+        INSERT INTO Producto (ID, Producto, Descripcion, PrecioVenta, CantidadMinima, Cantidad, IdCategoria, IdProveedor)
+        VALUES (p_IdProducto, p_Producto, p_Descripcion, p_PrecioVenta, p_CantidadMinima, p_Cantidad, p_IdCategoria, p_IdProveedor);
+        
+        -- Asignar el cursor de salida
+        OPEN p_Cursor FOR
+            SELECT * FROM Producto WHERE ID = p_IdProducto;
+        COMMIT;
+    EXCEPTION
+        WHEN OTHERS THEN
+            ROLLBACK;
+            RAISE;
+    END sp_Insert_Producto;
 
 --------------------------------------------------------------------------------
 ----------------------- PROCEDIMIENTO ALMACENADO UPDATE ------------------------
 --------------------------------------------------------------------------------
 CREATE OR REPLACE PROCEDURE sp_Update_Producto (
-    p_OldIdProducto IN INT,
-    p_NewIdProducto IN INT,
-    p_Producto IN VARCHAR,
-    p_Descripcion IN VARCHAR,
-    p_PrecioVenta IN INT,
-    p_CantidadMinima IN INT,
-    p_Cantidad IN INT,
-    p_IdCategoria IN INT,
-    p_IdProveedor IN INT
-) AS
-BEGIN
-    UPDATE Producto
-    SET
-        ID = p_NewIdProducto,
-        Producto = p_Producto,
-        Descripcion = p_Descripcion,
-        PrecioVenta = p_PrecioVenta,
-        CantidadMinima = p_CantidadMinima,
-        Cantidad = p_Cantidad,
-        IdCategoria = p_IdCategoria,
-        IdProveedor = p_IdProveedor
-    WHERE
-        ID = p_OldIdProducto;
-    
-    COMMIT;
-EXCEPTION
-    WHEN OTHERS THEN
-        ROLLBACK;
-        RAISE;
-END sp_Update_Producto;
+        p_Cursor OUT SYS_REFCURSOR,
+        p_OldIdProducto IN INT,
+        p_NewIdProducto IN INT,
+        p_Producto IN VARCHAR,
+        p_Descripcion IN VARCHAR,
+        p_PrecioVenta IN INT,
+        p_CantidadMinima IN INT,
+        p_Cantidad IN INT,
+        p_IdCategoria IN INT,
+        p_IdProveedor IN INT
+    ) AS
+    BEGIN
+        UPDATE Producto
+        SET
+            ID = p_NewIdProducto,
+            Producto = p_Producto,
+            Descripcion = p_Descripcion,
+            PrecioVenta = p_PrecioVenta,
+            CantidadMinima = p_CantidadMinima,
+            Cantidad = p_Cantidad,
+            IdCategoria = p_IdCategoria,
+            IdProveedor = p_IdProveedor
+        WHERE
+            ID = p_OldIdProducto;
+        
+        -- Asignar el cursor de salida
+        OPEN p_Cursor FOR
+            SELECT * FROM Producto WHERE ID = p_NewIdProducto;
+            
+        COMMIT;
+    EXCEPTION
+        WHEN OTHERS THEN
+            ROLLBACK;
+            RAISE;
+    END sp_Update_Producto;
 
 -------------------------------------------------------------------------------
 ------------------ PROCEDIMIENTO ALMACENADO DELETE ----------------------------
 -------------------------------------------------------------------------------
-CREATE OR REPLACE PROCEDURE sp_Delete_Producto (
-    p_IdProducto IN INT
-) AS
-BEGIN
-    DELETE FROM Producto
-    WHERE ID = p_IdProducto;
+CREATE OR REPLACE PROCEDURE sp_Delete_Producto (p_Cursor OUT SYS_REFCURSOR, p_IdProducto IN INT) AS
+    BEGIN
+        -- Guardar datos del producto antes de eliminarlo
+        OPEN p_Cursor FOR
+            SELECT * FROM Producto WHERE ID = p_IdProducto;
+            
+        DELETE FROM Producto
+        WHERE ID = p_IdProducto;
 
-    COMMIT;
-EXCEPTION
-    WHEN NO_DATA_FOUND THEN
-        DBMS_OUTPUT.PUT_LINE('No se encontró un producto con el ID ' || p_IdProducto);
-    WHEN OTHERS THEN
-        ROLLBACK;
-        RAISE;
-END sp_Delete_Producto;
+        COMMIT;
+    EXCEPTION
+        WHEN OTHERS THEN
+            ROLLBACK;
+            RAISE;
+    END sp_Delete_Producto;
