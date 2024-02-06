3617b0f2545a87a0a974e643fbdc4dc9cde4a97e
unknown <yanethykm@gmail.com>
2024-02-06 16:59:44 -0500

    Realizando modificaciones en los scripts de paquetes y procedimientos almancenados realizando una agregación de cursores para la muestra de datos en las capas de la tabla Cliente

--- PQ_CLIENTE_PACKAGE.sql
+++ PQ_CLIENTE_PACKAGE.sql
@@ -1,16 +1,16 @@
 
 CREATE OR REPLACE PACKAGE pq_Cliente_Package AS
   -- Procedimiento para insertar una nueva categoría
-  PROCEDURE sp_Insert_Cliente (p_IdPersona IN INT);
+  PROCEDURE sp_Insert_Cliente(p_Cursor OUT SYS_REFCURSOR, p_IdPersona IN INT);
 
   -- Procedimiento para listar todas las categorías
-  PROCEDURE sp_List_Cliente_DDL;
+  PROCEDURE sp_List_Cliente_DDL (p_Cursor OUT SYS_REFCURSOR);
 
   -- Procedimiento para actualizar una categoría por ID
-  PROCEDURE sp_Update_Cliente (p_IDCliente IN INT, p_IdPersona IN INT);
+  PROCEDURE sp_Update_Cliente(p_Cursor OUT SYS_REFCURSOR, p_IDCliente IN INT, p_IdPersona IN INT);
 
   -- Procedimiento para eliminar una categoría por ID
-  PROCEDURE sp_Delete_Cliente (p_IDCliente IN INT);
+  PROCEDURE sp_Delete_Cliente(p_Cursor OUT SYS_REFCURSOR, p_IDCliente IN INT);
 END pq_Cliente_Package;
 
 ----------------------------------------------------------------------------
@@ -19,37 +19,35 @@ END pq_Cliente_Package;
 
 CREATE OR REPLACE PACKAGE BODY pq_Cliente_Package AS
   -- Procedimiento para insertar un nuevo cliente
-  PROCEDURE sp_Insert_Cliente ( p_IdPersona IN INT ) AS
+  PROCEDURE sp_Insert_Cliente(p_Cursor OUT SYS_REFCURSOR, p_IdPersona IN INT) AS
     BEGIN
         INSERT INTO Cliente (IdPersona)
         VALUES (p_IdPersona);
+        
+        OPEN p_Cursor FOR
+            SELECT * FROM Cliente WHERE IdPersona = p_IdPersona;
+    
         COMMIT;
     EXCEPTION
         WHEN OTHERS THEN
             ROLLBACK;
             RAISE;
-END sp_Insert_Cliente;
+    END sp_Insert_Cliente;
+
 
   -- Procedimiento para listar todos los clientes
-  PROCEDURE sp_List_Cliente_DDL AS
+  PROCEDURE sp_List_Cliente_DDL (p_Cursor OUT SYS_REFCURSOR)AS
     BEGIN
-        FOR ClienteRow IN (
+        Open p_Cursor FOR
             SELECT 
+                p.id,
                 P.nit_cedula as "Identificacion", 
                 P.nombre_razonsocial as "Nombre/Razon Social", 
                 P.apellido as "Apellido", 
                 P.telefono as "Telefono", 
                 P.email as "Correo electronico"
         FROM Cliente C
-        INNER JOIN Persona P on C.idpersona = P.id
-        ) LOOP
-            DBMS_OUTPUT.PUT_LINE('Identificacion: ' || ClienteRow."Identificacion");
-            DBMS_OUTPUT.PUT_LINE('Nombre/Razon Social: ' || ClienteRow."Nombre/Razon Social");
-            DBMS_OUTPUT.PUT_LINE('Apellido: ' || ClienteRow."Apellido");
-            DBMS_OUTPUT.PUT_LINE('Telefono: ' || ClienteRow."Telefono");
-            DBMS_OUTPUT.PUT_LINE('Correo electronico: ' || ClienteRow."Correo electronico");
-            DBMS_OUTPUT.PUT_LINE('-----------------------');
-        END LOOP;
+        INNER JOIN Persona P on C.idpersona = P.id;
     EXCEPTION
         WHEN NO_DATA_FOUND THEN
             DBMS_OUTPUT.PUT_LINE('No se encontraron Clientes.');
@@ -59,30 +57,36 @@ END sp_Insert_Cliente;
 END sp_List_Cliente_DDL;
 
   -- Procedimiento para actualizar un cliente
-  PROCEDURE sp_Update_Cliente (p_IDCliente IN INT, p_IdPersona IN INT) AS
+  PROCEDURE sp_Update_Cliente(p_Cursor OUT SYS_REFCURSOR, p_IDCliente IN INT, p_IdPersona IN INT) AS
     BEGIN
+
         UPDATE Cliente
         SET IdPersona = p_IdPersona
+        WHERE ID = p_IDCliente;
+
+        OPEN p_Cursor FOR
+            SELECT * FROM Cliente WHERE ID = p_IDCliente;
+    
+        COMMIT;
+    EXCEPTION
+        WHEN OTHERS THEN
+            ROLLBACK;
+            RAISE;
+    END sp_Update_Cliente;
+
+
+  -- Procedimiento para eliminar un cliente
+    PROCEDURE sp_Delete_Cliente(p_Cursor OUT SYS_REFCURSOR, p_IDCliente IN INT) AS
+    BEGIN
+
+        OPEN p_Cursor FOR
+            SELECT * FROM Cliente WHERE ID = p_IDCliente;
+    
+        DELETE FROM Cliente
         WHERE ID = p_IDCliente;
     
         COMMIT;
     EXCEPTION
-        WHEN NO_DATA_FOUND THEN
-            DBMS_OUTPUT.PUT_LINE('No se encontró un cliente con el ID ' || p_IDCliente);
-        WHEN OTHERS THEN
-            ROLLBACK;
-            RAISE;
-END sp_Update_Cliente;
-
-  -- Procedimiento para eliminar un cliente
- PROCEDURE sp_Delete_Cliente (p_IDCliente IN INT) AS
-    BEGIN
-        DELETE FROM Cliente
-        WHERE ID = p_IDCliente;
-    COMMIT;
-    EXCEPTION
-        WHEN NO_DATA_FOUND THEN
-            DBMS_OUTPUT.PUT_LINE('No se encontró un cliente con el ID ' || p_IdCliente);
         WHEN OTHERS THEN
             ROLLBACK;
             RAISE;
--- SP_CRUD_TBL_CLIENTE.sql
+++ SP_CRUD_TBL_CLIENTE.sql
@@ -1,66 +1,79 @@
 --------------------------------------------------------
 ----------- PROCEDIMIENTO ALMACENADO INSERT ------------
 --------------------------------------------------------
-CREATE OR REPLACE PROCEDURE sp_Insert_Cliente (
-    p_IdPersona IN INT
-) AS
-BEGIN
-    INSERT INTO Cliente (IdPersona)
-    VALUES (p_IdPersona);
-
-    COMMIT;
-EXCEPTION
-    WHEN OTHERS THEN
-        ROLLBACK;
-        RAISE;
+CREATE OR REPLACE PROCEDURE sp_Insert_Cliente(p_Cursor OUT SYS_REFCURSOR, p_IdPersona IN INT) AS
+    BEGIN
+        INSERT INTO Cliente (IdPersona)
+        VALUES (p_IdPersona);
+        
+        OPEN p_Cursor FOR
+            SELECT * FROM Cliente WHERE IdPersona = p_IdPersona;
+    
+        COMMIT;
+    EXCEPTION
+        WHEN OTHERS THEN
+            ROLLBACK;
+            RAISE;
 END sp_Insert_Cliente;
 
 --------------------------------------------------------
 ----------- PROCEDIMIENTO ALMACENADO DELETE ------------
 --------------------------------------------------------
-CREATE OR REPLACE PROCEDURE sp_Delete_Cliente (
-    p_IDCliente IN INT
-) AS
-BEGIN
-    DELETE FROM Cliente
-    WHERE ID = p_IDCliente;
+CREATE OR REPLACE PROCEDURE sp_Delete_Cliente(p_Cursor OUT SYS_REFCURSOR, p_IDCliente IN INT) AS
+    BEGIN
+
+        OPEN p_Cursor FOR
+            SELECT * FROM Cliente WHERE ID = p_IDCliente;
     
-    COMMIT;
-EXCEPTION
-    WHEN NO_DATA_FOUND THEN
-        DBMS_OUTPUT.PUT_LINE('No se encontró un cliente con el ID ' || p_IdCliente);
-    WHEN OTHERS THEN
-        ROLLBACK;
-        RAISE;
+        DELETE FROM Cliente
+        WHERE ID = p_IDCliente;
+    
+        COMMIT;
+    EXCEPTION
+        WHEN OTHERS THEN
+            ROLLBACK;
+            RAISE;
 END sp_Delete_Cliente;
 
 -------------------------------------------------------------------------------
 --------------- PROCEDIMIENTO ALMACENADO UPDATE -------------------------------
 -------------------------------------------------------------------------------
-CREATE OR REPLACE PROCEDURE sp_Update_Cliente (
-    p_IDCliente IN INT,
-    p_IdPersona IN INT
-) AS
-BEGIN
-    UPDATE Cliente
-    SET IdPersona = p_IdPersona
-    WHERE ID = p_IDCliente;
+CREATE OR REPLACE PROCEDURE sp_Update_Cliente(p_Cursor OUT SYS_REFCURSOR, p_IDCliente IN INT, p_IdPersona IN INT) AS
+    BEGIN
 
-    COMMIT;
-EXCEPTION
-    WHEN NO_DATA_FOUND THEN
-        DBMS_OUTPUT.PUT_LINE('No se encontró un cliente con el ID ' || p_IDCliente);
-    WHEN OTHERS THEN
-        ROLLBACK;
-        RAISE;
-END sp_Update_Cliente;
+        UPDATE Cliente
+        SET IdPersona = p_IdPersona
+        WHERE ID = p_IDCliente;
+
+        OPEN p_Cursor FOR
+            SELECT * FROM Cliente WHERE ID = p_IDCliente;
+    
+        COMMIT;
+    EXCEPTION
+        WHEN OTHERS THEN
+            ROLLBACK;
+            RAISE;
+    END sp_Update_Cliente;
 
 -------------------------------------------------------------------------------
 --------------- PROCEDIMIENTO ALMACENADO LIST ---------------------------------
 -------------------------------------------------------------------------------
-CREATE OR REPLACE PROCEDURE sp_List_Cliente AS
-BEGIN
-    FOR ClienteRow IN (SELECT IdPersona FROM Cliente) LOOP
-        DBMS_OUTPUT.PUT_LINE('ID de la Persona relacionada: ' || ClienteRow.IdPersona);
-    END LOOP;
-END sp_List_Cliente;
+CREATE OR REPLACE PROCEDURE sp_List_Cliente_DDL (p_Cursor OUT SYS_REFCURSOR)AS
+    BEGIN
+        Open p_Cursor FOR
+            SELECT 
+                p.id,
+                P.nit_cedula as "Identificacion", 
+                P.nombre_razonsocial as "Nombre/Razon Social", 
+                P.apellido as "Apellido", 
+                P.telefono as "Telefono", 
+                P.email as "Correo electronico"
+        FROM Cliente C
+        INNER JOIN Persona P on C.idpersona = P.id;
+    EXCEPTION
+        WHEN NO_DATA_FOUND THEN
+            DBMS_OUTPUT.PUT_LINE('No se encontraron Clientes.');
+        WHEN OTHERS THEN
+            ROLLBACK;
+            RAISE;
+END sp_List_Cliente_DDL;
