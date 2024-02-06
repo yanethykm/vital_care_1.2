2a790753823db1b45b73e591ccb5b21814448570
unknown <yanethykm@gmail.com>
2024-02-06 16:49:25 -0500

    Realizando modificaciones en los scripts de paquetes y procedimientos almancenados realizando una agregación de cursores para la muestra de datos en las capas

--- PQ_TBL_CATEGORIA_PACKAGE.sql
+++ PQ_TBL_CATEGORIA_PACKAGE.sql
@@ -1,15 +1,15 @@
 CREATE OR REPLACE PACKAGE pq_Categoria_Package AS
   -- Procedimiento para insertar una nueva categoría
-  PROCEDURE sp_Insert_Categoria(p_Categoria IN VARCHAR2);
+  PROCEDURE sp_Insert_Categoria(p_Cursor OUT SYS_REFCURSOR, p_Categoria IN VARCHAR2);
 
   -- Procedimiento para listar todas las categorías
-  PROCEDURE sp_List_Categoria;
+  PROCEDURE sp_List_Categoria (p_Cursor OUT SYS_REFCURSOR);
 
   -- Procedimiento para actualizar una categoría por ID
-  PROCEDURE sp_Update_Categoria(p_IDCategoria IN INT, p_Categoria IN VARCHAR2);
+  PROCEDURE sp_Update_Categoria(p_Cursor OUT SYS_REFCURSOR, p_IDCategoria IN INT, p_Categoria IN VARCHAR2);
 
   -- Procedimiento para eliminar una categoría por ID
-  PROCEDURE sp_Delete_Categoria(p_IDCategoria IN INT);
+  PROCEDURE sp_Delete_Categoria(p_Cursor OUT SYS_REFCURSOR, p_IDCategoria IN INT);
 END pq_Categoria_Package;
 -------------------------------------------------------------------------------
 -------------------------------------------------------------------------------
@@ -17,38 +17,62 @@ END pq_Categoria_Package;
 
 CREATE OR REPLACE PACKAGE BODY pq_Categoria_Package AS
   -- Implementación del procedimiento para insertar una nueva categoría
-  PROCEDURE sp_Insert_Categoria(p_Categoria IN VARCHAR2) IS
-  BEGIN
-    INSERT INTO Categoria (Categoria)
-    VALUES (p_Categoria);
-    COMMIT;
-  END sp_Insert_Categoria;
+    PROCEDURE sp_Insert_Categoria(p_Cursor OUT SYS_REFCURSOR, p_Categoria IN VARCHAR2) IS
+    BEGIN
+        INSERT INTO Categoria (Categoria)
+        VALUES (p_Categoria);
+        
+        OPEN p_Cursor FOR
+            SELECT * FROM Categoria WHERE Categoria = p_Categoria;
+    
+        COMMIT;
+    EXCEPTION
+        WHEN OTHERS THEN
+            ROLLBACK;
+            RAISE;
+    END sp_Insert_Categoria;
 
   -- Implementación del procedimiento para listar todas las categorías
-  PROCEDURE sp_List_Categoria IS
+  PROCEDURE sp_List_Categoria (p_Cursor OUT SYS_REFCURSOR) IS
   BEGIN
-    FOR CategoriaRow IN (SELECT Categoria FROM Categoria) 
-    LOOP
-        DBMS_OUTPUT.PUT_LINE('Categoria: ' || CategoriaRow.Categoria);
-        DBMS_OUTPUT.PUT_LINE('-----------------------');
-    END LOOP;
+    OPEN p_Cursor FOR SELECT Categoria FROM Categoria; 
   END sp_List_Categoria;
 
   -- Implementación del procedimiento para actualizar una categoría por ID
-  PROCEDURE sp_Update_Categoria(p_IDCategoria IN INT, p_Categoria IN VARCHAR2) IS
-  BEGIN
-    UPDATE Categoria
-    SET Categoria = p_Categoria
-    WHERE ID = p_IDCategoria;
-    COMMIT;
-  END sp_Update_Categoria;
+  PROCEDURE sp_Update_Categoria(p_Cursor OUT SYS_REFCURSOR, p_IDCategoria IN INT, p_Categoria IN VARCHAR2) IS
+    BEGIN
+        -- Actualizar la categoría
+        UPDATE Categoria
+        SET Categoria = p_Categoria
+        WHERE ID = p_IDCategoria;
+    
+        -- Abrir el cursor de salida con los datos de la categoría actualizada
+        OPEN p_Cursor FOR
+            SELECT * FROM Categoria WHERE ID = p_IDCategoria;
+    
+        COMMIT;
+    EXCEPTION
+        WHEN OTHERS THEN
+            ROLLBACK;
+            RAISE;
+    END sp_Update_Categoria;
 
   -- Implementación del procedimiento para eliminar una categoría por ID
-  PROCEDURE sp_Delete_Categoria(p_IDCategoria IN INT) IS
-  BEGIN
-    DELETE FROM Categoria
-    WHERE ID = p_IDCategoria;
-    COMMIT;
-  END sp_Delete_Categoria;
+  PROCEDURE sp_Delete_Categoria(p_Cursor OUT SYS_REFCURSOR, p_IDCategoria IN INT) IS
+    BEGIN
+        -- Guardar datos de la categoría antes de eliminarla
+        OPEN p_Cursor FOR
+            SELECT * FROM Categoria WHERE ID = p_IDCategoria;
+    
+        -- Eliminar la categoría
+        DELETE FROM Categoria
+        WHERE ID = p_IDCategoria;
+    
+        COMMIT;
+    EXCEPTION
+        WHEN OTHERS THEN
+            ROLLBACK;
+            RAISE;
+    END sp_Delete_Categoria;
 END pq_Categoria_Package;
 
--- SP_CRUD_TBL_CATEGORIA.sql
+++ SP_CRUD_TBL_CATEGORIA.sql
@@ -1,67 +1,66 @@
 --------------------------------------------------------
 ----------- PROCEDIMIENTO ALMACENADO INSERT ------------
 --------------------------------------------------------
-CREATE OR REPLACE PROCEDURE sp_Insert_Categoria (
-    p_Categoria IN VARCHAR
-) AS
-BEGIN
-    INSERT INTO Categoria (Categoria)
-    VALUES (p_Categoria);
+CREATE OR REPLACE PROCEDURE sp_Insert_Categoria(p_Cursor OUT SYS_REFCURSOR, p_Categoria IN VARCHAR2) IS
+    BEGIN
+        INSERT INTO Categoria (Categoria)
+        VALUES (p_Categoria);
+        
+        OPEN p_Cursor FOR
+            SELECT * FROM Categoria WHERE Categoria = p_Categoria;
     
-    COMMIT;
-EXCEPTION
-    WHEN OTHERS THEN
-        ROLLBACK;
-        RAISE;
+        COMMIT;
+    EXCEPTION
+        WHEN OTHERS THEN
+            ROLLBACK;
+            RAISE;
 END sp_Insert_Categoria;
 
 --------------------------------------------------------
 ----------- PROCEDIMIENTO ALMACENADO DELETE ------------
 --------------------------------------------------------
-CREATE OR REPLACE PROCEDURE sp_Delete_Categoria (
-    p_IDCategoria IN INT
-) AS
-BEGIN
-    DELETE FROM Categoria
-    WHERE ID = p_IDCategoria;
+CREATE OR REPLACE PROCEDURE sp_Delete_Categoria(p_Cursor OUT SYS_REFCURSOR, p_IDCategoria IN INT) IS
+    BEGIN
+        -- Guardar datos de la categoría antes de eliminarla
+        OPEN p_Cursor FOR
+            SELECT * FROM Categoria WHERE ID = p_IDCategoria;
     
-    COMMIT;
-EXCEPTION
-    WHEN NO_DATA_FOUND THEN
-        DBMS_OUTPUT.PUT_LINE('No se encontró una categoría con el ID ' || p_IDCategoria);
-    WHEN OTHERS THEN
-        ROLLBACK;
-        RAISE;
+        -- Eliminar la categoría
+        DELETE FROM Categoria
+        WHERE ID = p_IDCategoria;
+    
+        COMMIT;
+    EXCEPTION
+        WHEN OTHERS THEN
+            ROLLBACK;
+            RAISE;
 END sp_Delete_Categoria;
 
 -------------------------------------------------------------------------------
 --------------- PROCEDIMIENTO ALMACENADO UPDATE -------------------------------
 -------------------------------------------------------------------------------
-CREATE OR REPLACE PROCEDURE sp_Update_Categoria (
-    p_IDCategoria IN INT,
-    p_Categoria IN VARCHAR
-) AS
-BEGIN
-    UPDATE Categoria
-    SET Categoria = p_Categoria
-    WHERE ID = p_IDCategoria;
-
-    COMMIT;
-EXCEPTION
-    WHEN NO_DATA_FOUND THEN
-        DBMS_OUTPUT.PUT_LINE('No se encontró una categoría con el ID ' || p_IDCategoria);
-    WHEN OTHERS THEN
-        ROLLBACK;
-        RAISE;
+CREATE OR REPLACE PROCEDURE sp_Update_Categoria(p_Cursor OUT SYS_REFCURSOR, p_IDCategoria IN INT, p_Categoria IN VARCHAR2) IS
+    BEGIN
+        -- Actualizar la categoría
+        UPDATE Categoria
+        SET Categoria = p_Categoria
+        WHERE ID = p_IDCategoria;
+    
+        -- Abrir el cursor de salida con los datos de la categoría actualizada
+        OPEN p_Cursor FOR
+            SELECT * FROM Categoria WHERE ID = p_IDCategoria;
+    
+        COMMIT;
+    EXCEPTION
+        WHEN OTHERS THEN
+            ROLLBACK;
+            RAISE;
 END sp_Update_Categoria;
 
 -------------------------------------------------------------------------------
 --------------- PROCEDIMIENTO ALMACENADO LIST ---------------------------------
 -------------------------------------------------------------------------------
-CREATE OR REPLACE PROCEDURE sp_List_Categoria AS
-BEGIN
-    FOR CategoriaRow IN (SELECT Categoria FROM Categoria) LOOP
-        DBMS_OUTPUT.PUT_LINE('Categoria: ' || CategoriaRow.Categoria);
-        DBMS_OUTPUT.PUT_LINE('-----------------------');
-    END LOOP;
-END sp_List_Categoria;
+CREATE OR REPLACE PROCEDURE sp_List_Categoria (p_Cursor OUT SYS_REFCURSOR) IS
+  BEGIN
+    OPEN p_Cursor FOR SELECT Categoria FROM Categoria; 
+  END sp_List_Categoria;
